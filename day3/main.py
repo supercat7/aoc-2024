b4d87acb8109ce8e4df2c586a4803fd83f9b7247
supercat7 <superuser3344@protonmail.com>
2025-02-07 16:32:17 -0500

    day3 part2 done

--- day3/main.py
+++ day3/main.py
@@ -16,4 +16,24 @@ def part1():
     print(sum)
         
 
+def part2():
+    allow = True
+    sum = 0
+
+    exp = re.finditer(r"(do\(\))|(don't\(\))|(mul\([0-9]+,[0-9]+\))", data)
+
+    for i in exp:
+        if i.group(1):
+            allow = True
+        elif i.group(2):
+            allow = False
+        elif i.group(3):
+            if allow:
+                num_arr = re.findall(r'\d+', i.group(3)) 
+                sum += int(num_arr[0]) * int(num_arr[1]) 
+
+    print(sum)
+
+
 part1()
+part2()
